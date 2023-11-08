52c2e7fb1f9a917210fb4dda3556af6be79b2458
abin <abinsasikumar22@gmail.com>
2023-11-08 11:33:18 +0530

    flexbox works

---
+++ november/javascript/8_11/script.js
@@ -0,0 +1,30 @@
+console.log("welcome to javascript hijo de puta")
+console.log(window)
+// var i;
+// i= "hello"
+// console.log("i : ",i)
+
+// var a= 100;
+// console.log("a : ",a)
+
+var str= "this is a string";
+console.log("str :",str);
+console.log("type of str : ", typeof str);
+
+var num= 1001011;
+console.log("num :",num);
+console.log("type of num : ", typeof num);
+
+var bool= true;
+console.log("bool :",bool);
+console.log("type of bool : ", typeof bool);
+
+var a1= undefined;
+console.log("a1 :",a1);
+console.log("type of a1 : ", typeof a1);
+
+var b1= null;
+console.log("b1 :",b1);
+console.log("type of b1 : ", typeof b1);
+
+document.write("cristiano ronaldo")
