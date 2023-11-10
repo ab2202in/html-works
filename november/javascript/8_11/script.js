e846866db91d07fe03d65ca0c912ca00b4c0aeae
abin <abinsasikumar22@gmail.com>
2023-11-10 11:28:04 +0530

    flexbox works

--- november/javascript/8_11/script.js
+++ november/javascript/8_11/script.js
@@ -1,5 +1,5 @@
-console.log("welcome to javascript hijo de puta")
-console.log(window)
+// console.log("welcome to javascript hijo de puta")
+// console.log(window)
 // var i;
 // i= "hello"
 // console.log("i : ",i)
@@ -29,27 +29,81 @@ console.log(window)
 
 // document.write("cristiano ronaldo")
 
-var str=" Hello,World "
+// var str=" Hello,World "
 
-console.log("length of the string : ",str.length);
-console.log("uppercase : ",str.toLocaleUpperCase());
-console.log("lowercase : ",str.toLocaleLowerCase());
-console.log(str.toUpperCase().length);
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
+// console.log("length of the string : ",str.length);
+// console.log("uppercase : ",str.toLocaleUpperCase());
+// console.log("lowercase : ",str.toLocaleLowerCase());
+// console.log(str.toUpperCase().length);
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
 
-console.log("trim :",str.trim());
-console.log("trimmed length :",str.trim().length);
-console.log("trimmed end of the string : ",str.trimEnd());
-console.log("trimmed start of the strong : ",str.trimStart());
+// console.log("trim :",str.trim());
+// console.log("trimmed length :",str.trim().length);
+// console.log("trimmed end of the string : ",str.trimEnd());
+// console.log("trimmed start of the strong : ",str.trimStart());
 
-console.log("trimmed end of the string : ",str.trimEnd().length);
-console.log("trimmed start of the strong : ",str.trimStart().length);
+// console.log("trimmed end of the string : ",str.trimEnd().length);
+// console.log("trimmed start of the strong : ",str.trimStart().length);
 
-console.log("substring : ",str.substring(1,3));
-console.log("replace :",str.replace("Hello","Chalo"));
-console.log("startswith : ",str.startsWith(" Hello"));
-console.log("endswith : ",str.endsWith("old"));
+// console.log("substring : ",str.substring(1,3));
+// console.log("replace :",str.replace("Hello","Chalo"));
+// console.log("startswith : ",str.startsWith(" Hello"));
+// console.log("endswith : ",str.endsWith("old"));
 
-console.log("split : ",str.split(","));
-console.log("join : ",str.split(",").join(","));
+// console.log("split : ",str.split(","));
+// console.log("join : ",str.split(",").join(","));
+
+// var arr = [10,20,30,40,50,'hai','hello',"hey",true,false,undefined,null];
+
+// console.log("arr : ", arr);
+// console.log("type of : ",typeof arr);
+// console.log("length of : ",arr.length);
+// console.log("index of hello : ",arr.indexOf('hello'));
+// console.log("index of 20 : ",arr.indexOf(20));
+// console.log("join : ",arr.join(''))
+// console.log("joined length : ",arr.join('').length);
+
+// console.log("push : ",arr.push("end"),",","arr : ", arr);
+// console.log("unshift : ",arr.unshift("first"),",","arr : ", arr);
+// console.log("pop : ",arr.pop(),",","arr : ", arr);
+// console.log("shift : ",arr.shift(),",","arr : ", arr);
+
+// arr[2]=35;
+// arr[3]=45;
+// arr[4]=55;
+// console.log("arr : ", arr);
+
+// arr.splice(10,2);
+// console.log("arr : ", arr);
+
+// arr.splice(4,0,50);
+// arr.splice(3,0,40);
+// arr.splice(2,0,30);
+// arr.splice(2,0,25);
+// arr.splice(1,0,15);
+// arr.splice(0,0,5);
+// console.log("arr : ", arr);
+
+
+var obj = {
+    firstName : "Abin",
+    secondName : "Sasikumar",
+    age : 21,
+    Indian : true,
+    hobbies : ["sleeping","playing","working"],
+}
+
+console.log("first name : ", obj.firstName);
+console.log("second name : ", obj.secondName);
+console.log("age : ", obj.age);
+console.log("indian? : ", obj.Indian);
+console.log("hobbies : ", obj.hobbies);
+
+obj.mark1=50;
+obj.mark2=70;
+obj.mark3=75;
+obj.age=51;
+
+console.log("obj : ",obj);
+
