8ac0b33ea530d48bce581471c8490b1c2c7804ea
abin sasikumar <abinsasikumar22@gmail.com>
2024-01-10 11:36:13 +0530

    mongod

--- 2023/december/21_12/client/script.js
+++ 2023/december/21_12/client/script.js
@@ -15,14 +15,15 @@ async function getData(){
         rows = rows+`
         <tr>
         <td>${parsedData[i]._id}</td>
-        <td>${parsedData[i].fname}</td>
-        <td>${parsedData[i].lname}</td>
-        <td>${parsedData[i].uname}</td>
-        <td>${parsedData[i].eMail}</td>
-        <td>${parsedData[i].pWord}</td>
-        <td>${parsedData[i].age}</td>
-        <td>${parsedData[i].place}</td>
-        <td>${parsedData[i].phn}</td>
+        <td><input type="text" name ="fname" id="fname-${parsedData[i]._id}" value="${parsedData[i].fname}" disabled = true></td>
+        <td><input type="text" name ="lname" id="lname-${parsedData[i]._id}" value ="${parsedData[i].lname}" disabled =true></td>
+        <td><input type="username" name ="uname" id="uname-${parsedData[i]._id}" value = "${parsedData[i].uname}" disabled = true></td>
+        <td><input type="email" name ="eMail" id="email-${parsedData[i]._id}" value = "${parsedData[i].eMail}" disabled = true></td>
+        <td><input type="password" name ="pWord" id="pword-${parsedData[i]._id}" value = "${parsedData[i].pWord}" disabled = true></td>
+        <td><input type="number" name ="age" id="age-${parsedData[i]._id}" value = "${parsedData[i].age}" disabled = true></td>
+        <td><input type="text" name ="place" id="place-${parsedData[i]._id}" value = "${parsedData[i].place}" disabled = true></td>
+        <td><input type="tel" name ="phn" id="phone-${parsedData[i]._id}" value = "${parsedData[i].phn}" disabled = true></td>
+        <td><button onClick="handleEdit('${parsedData[i]._id}')">EDIT</button></td>
         </tr>
         `
     }
@@ -31,3 +32,86 @@ async function getData(){
 }
 
 getData();
+
+function handleEdit(id){
+
+
+    let fname =document.getElementById(`fname-${id}`);
+    fname.disabled =false;
+
+    let lname =document.getElementById(`lname-${id}`);
+    lname.disabled =false;
+
+    let uname =document.getElementById(`uname-${id}`);
+    uname.disabled =false;
+
+    let email =document.getElementById(`email-${id}`);
+    email.disabled =false;
+
+    let pword =document.getElementById(`pword-${id}`);
+    pword.disabled =false;
+
+    let age =document.getElementById(`age-${id}`);
+    age.disabled =false;
+
+    let place =document.getElementById(`place-${id}`);
+    place.disabled =false;
+
+    let phone =document.getElementById(`phone-${id}`);
+    phone.disabled =false;
+}
+
+async function handleSave(id){
+
+
+    let fnameTag =document.getElementById(`fname-${id}`);
+    let fname = fnameTag.value;
+
+    let lnameTag =document.getElementById(`lname-${id}`);
+    let lname = lnameTag.value;
+
+    let unameTag =document.getElementById(`uname-${id}`);
+    let uname = unameTag.value;
+
+    let emailTag =document.getElementById(`email-${id}`);
+    let email = emailTag.value;
+
+    let pwordTag =document.getElementById(`pword-${id}`);
+    let pword = pwordTag.value;
+
+    let ageTag =document.getElementById(`age-${id}`);
+    let age = ageTag.value;
+
+    let placeTag =document.getElementById(`place-${id}`);
+    let place = placeTag.value;
+
+    let phoneTag =document.getElementById(`phone-${id}`);
+    let phone = phoneTag.value;
+
+    let data ={
+        id,
+        fname,
+        lname,
+        uname,
+        email,
+        pword,
+        age,
+        place,
+        phone,
+
+    }
+
+    let jsonData =JSON.stringify(data);
+    
+    let response = await fetch('http://localhost:3000/editData',{
+        method : "PUT",
+        headers : {
+            "Content-Type" : "applictaion/json",
+        },
+        body : jsonData,
+    });
+
+
+
+    
+}
